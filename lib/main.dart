e29b531b0dc09a7cfd2dfa81c5987096197e5e2b
Dulaj <shad3s.of.m3@gmail.com>
2022-04-15 15:55:52 +0530

    make cards nagivatable

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -30,153 +33,200 @@ class MyApp extends StatelessWidget {
           ),
           body: ListView(
             children: [
-              SizedBox(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            const Expanded(
-                              flex: 3,
-                              child: Text(
-                                "Flutter Video 2",
-                                style: TextStyle(fontSize: 25),
-                              ),
-                            ),
-                            Expanded(
+              GestureDetector(
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Expanded(
+                                flex: 3,
                                 child: Text(
-                              "Flutter Project",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.lightBlue[600]),
-                            ))
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
-                        const Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text("8.00AM - 8.30AM"),
-                        )
-                      ],
+                                  "Flutter Video 2",
+                                  style: TextStyle(fontSize: 25),
+                                ),
+                              ),
+                              Expanded(
+                                  child: Text(
+                                "Flutter Project",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.lightBlue[600]),
+                              ))
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("8.00AM - 8.30AM"),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            const Expanded(
-                              flex: 3,
-                              child: Text(
-                                "Spanish Lesson 3",
-                                style: TextStyle(fontSize: 25),
-                              ),
-                            ),
-                            Expanded(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SecondScreen(),
+                        ));
+                  }),
+              GestureDetector(
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Expanded(
+                                flex: 3,
                                 child: Text(
-                              "Spanish Project",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.red[600]),
-                            ))
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
-                        const Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text("10.00AM - 10.30AM"),
-                        )
-                      ],
+                                  "Spanish Lesson 3",
+                                  style: TextStyle(fontSize: 25),
+                                ),
+                              ),
+                              Expanded(
+                                  child: Text(
+                                "Spanish Project",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red[600]),
+                              ))
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("10.00AM - 10.30AM"),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            const Expanded(
-                              flex: 3,
-                              child: Text(
-                                "Ideation",
-                                style: TextStyle(fontSize: 25),
-                              ),
-                            ),
-                            Expanded(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SecondScreen(),
+                        ));
+                  }),
+              GestureDetector(
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Expanded(
+                                flex: 3,
                                 child: Text(
-                              "Portfolio Project",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.yellow[600]),
-                            ))
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
-                        const Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text("3.00PM - 3.30PM"),
-                        )
-                      ],
+                                  "Ideation",
+                                  style: TextStyle(fontSize: 25),
+                                ),
+                              ),
+                              Expanded(
+                                  child: Text(
+                                "Portfolio Project",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.yellow[600]),
+                              ))
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("3.00PM - 3.30PM"),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            const Expanded(
-                              flex: 3,
-                              child: Text(
-                                "Flexbox Challenge 2",
-                                style: TextStyle(fontSize: 25),
-                              ),
-                            ),
-                            Expanded(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SecondScreen(),
+                        ));
+                  }),
+              GestureDetector(
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Expanded(
+                                flex: 3,
                                 child: Text(
-                              "CSS Project",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.green[600]),
-                            ))
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
-                        const Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text("12.00AM - 12.30AM"),
-                        )
-                      ],
+                                  "Flexbox Challenge 2",
+                                  style: TextStyle(fontSize: 25),
+                                ),
+                              ),
+                              Expanded(
+                                  child: Text(
+                                "CSS Project",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.green[600]),
+                              ))
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text("12.00AM - 12.30AM"),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SecondScreen(),
+                        ));
+                  }),
             ],
             padding: const EdgeInsets.all(10),
           ),
         ));
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Second Route'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go back!'),
+          ),
+        ),
+      ),
+    );
   }
 }
